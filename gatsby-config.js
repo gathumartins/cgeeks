e08d1ea38b960e4b878971eab6160689164d2780
gathumartins <gathumartins@gmail.com>
2022-03-16 14:43:10 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "https://backend.creativegeeks.co.ke/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
