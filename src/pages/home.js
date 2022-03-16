b3dd92872097831e3a4f70fa13d081856b674ff2
gathumartins <gathumartins@gmail.com>
2022-03-16 17:30:11 +0300

    modified homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
 
 function home() {
   return (
-    <div>home</div>
+    <Container fluid={'lg'}>
+    <h1>This is the home page</h1>
+    </Container>
   )
 }
 
