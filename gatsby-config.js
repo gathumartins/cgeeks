2a39dc9dfe653603d80940e9c6b37990a378dd6b
gathumartins <gathumartins@gmail.com>
2022-03-18 14:29:45 +0300

    updadted coming soon page

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
     resolve: 'gatsby-source-wordpress',
     options: {
-      "url": "https://new.iaccess.co.ke/graphql"
+      "url": "https://back.creativegeeks.co.ke/graphql"
     }
   }, 
   "gatsby-plugin-image", 
